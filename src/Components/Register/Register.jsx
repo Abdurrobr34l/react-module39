921d54bbdfc81d2b70ac73ad8fa3dfd552065f16
AbdurRob <abdurrobr34l@gmail.com>
2025-10-15 12:27:05 +0600

    Created Register page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Register = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <title>Module-49 | Register</title>
 
-      Register
+      <form>
+        <fieldset className="fieldset p-6 w-[335px] bg-base-200 border border-blue-600 rounded-box md:w-[450px]">
+          <legend className="fieldset-legend text-3xl text-blue-600">Register Now!</legend>
+
+          <label className="label">Email</label>
+          <input type="email" className="input w-full" placeholder="Email" />
+
+          <label className="label mt-4">Password</label>
+          <input type="password" className="input w-full" placeholder="Password" />
+
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
     </div>
   );
 };
