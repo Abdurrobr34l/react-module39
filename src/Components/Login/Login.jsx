1c3b8941a2d77e2344032a98247ad1d17a31f93f
AbdurRob <abdurrobr34l@gmail.com>
2025-10-15 12:48:41 +0600

    Created Login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <title>Module-49 | Login</title>
 
-      Login
+      {/* Form */}
+      <form>
+        <fieldset className="fieldset p-6 w-[335px] bg-base-200 border border-blue-600 rounded-box md:w-[450px]">
+          <legend className="fieldset-legend text-3xl text-blue-600">
+            Login
+          </legend>
+
+          <label className="label">Email</label>
+          <input type="email" className="input w-full" placeholder="Email" />
+
+          <label className="label mt-4">Password</label>
+          <input
+            type="password"
+            className="input w-full"
+            placeholder="Password"
+          />
+
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
     </div>
   );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
     <div className="flex items-center justify-center">
       <title>Module-49 | Register</title>
 
+      {/* Form */}
       <form>
         <fieldset className="fieldset p-6 w-[335px] bg-base-200 border border-blue-600 rounded-box md:w-[450px]">
-          <legend className="fieldset-legend text-3xl text-blue-600">Register Now!</legend>
+          <legend className="fieldset-legend text-3xl text-blue-600">
+            Register Now!
+          </legend>
 
           <label className="label">Email</label>
           <input type="email" className="input w-full" placeholder="Email" />
 
           <label className="label mt-4">Password</label>
-          <input type="password" className="input w-full" placeholder="Password" />
+          <input
+            type="password"
+            className="input w-full"
+            placeholder="Password"
+          />
 
           <button className="btn btn-neutral mt-4">Register</button>
+
+          {/* Go to Login page if have account */}
+          <div className="mt-3">
+            <p className="textarea-md">
+            Already have an acount? Please{" "}
+            <Link to={"/login"} className="font-medium text-blue-600 hover:text-blue-50">
+              Login
+            </Link>
+          </p>
+          </div>
         </fieldset>
       </form>
     </div>
